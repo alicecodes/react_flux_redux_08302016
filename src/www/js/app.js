b12bcf264015171e40adeccef62bead5081c2084
Eric W. Greene <eric@ericwgreene.com>
2016-09-01 11:09:38 -0700

    added first reducer

--- src/www/js/app.js
+++ src/www/js/app.js
@@ -1,38 +1,43 @@
 import 'bootstrap-loader';
 import '../css/styles.scss';
 
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import keyMirror from 'key-mirror';
 
-// function ajax(options) {
+const actionTypes = keyMirror({
+	APPEND_ITEM: null
+});
 
-// 	return new Promise(function(resolve, reject) {
+const reducer = (state = { items: [] }, action) => {
 
-// 		const xhr = new XMLHttpRequest();
+	let newState = state;
 
-// 		xhr.addEventListener('readystatechange', function() {
-// 			if (xhr.readyState === 4 && xhr.status === 200) {
-// 				resolve(JSON.parse(xhr.responseText));
-// 			}
-// 		});
+	switch (action.type) {
+		case actionTypes.APPEND_ITEM:
+			newState = Object.assign({}, state, { items: state.items.concat(action.item) });
+	}
 
-// 		xhr.open(options.method, options.url);
-// 		xhr.send();
+	console.log('New State -----');
+	console.log(JSON.stringify(newState));
 
-// 	});
+	return newState;
+};
 
-// }
+[
+	{ type: actionTypes.APPEND_ITEM, item: { id: 1, name: 'Item 1'}}, // action
+	{ type: actionTypes.APPEND_ITEM, item: { id: 2, name: 'Item 2'}}, // action
+	{ type: actionTypes.DELETE_ITEM, itemId: 1 }, // implement this action in the system
+	{ type: actionTypes.APPEND_ITEM, item: { id: 3, name: 'Item 3'}}, // action
+].reduce(reducer, undefined);
 
-// ajax({
-// 	method: 'GET',
-// 	url: '/api/widgets'
-// }).then(function(results) {
-// 	console.dir(results);
-// });
 
-fetch('/api/widgets')
-	.then(res => res.json())
-	.then(results => console.dir(results));
+
+// const v = [1,2,3,4,5].reduce(function(state, action) {
+// 	console.log('state: ', state, ', action:', action);
+// 	return state + action;
+// }, 0);
+
+// console.log(v);
+
 
 
 
